c9af31b61371a22cb9544532dd9bc048424c0a68
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-16 17:10:56 +0800

    templates added

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('home page')
+    return render(request,'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('Product Page')
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse('Customer Page')
+    return render(request, 'accounts/customer.html')
 
