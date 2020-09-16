bf9380bdef35473a1d41b25cc9ef5fc83b26e24f
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-16 16:42:33 +0800

    urls set

---
+++ crm/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('home page')
+
+def products(request):
+    return HttpResponse('Product Page')
+
+def customer(request):
+    return HttpResponse('Customer Page')
+
