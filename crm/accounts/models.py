622a8e2df162c80c58a2843792c2f2ca2bf188d9
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-21 21:51:24 +0800

    models update

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -30,7 +30,7 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    #customer = 
-    #product = 
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
