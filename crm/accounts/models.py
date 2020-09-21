2f1e4fe3b0864533d4cd3783a3b7ab72f50f681d
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-09-21 22:44:33 +0800

    models update

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -11,6 +11,14 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -22,12 +30,8 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
 
-class Tag(models.Model):
-    name = models.CharField(max_length=200, null=True)
-
-    def __str__(self):
-        return self.name
 
 class Order(models.Model):
     STATUS = (
@@ -39,4 +43,4 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-    tags = models.ManyToManyField(Tag)
+    
